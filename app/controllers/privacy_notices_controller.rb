da45a55ec20b1d89cf68ae472589eba18c680397
Arthur <a.dhuy@orange.fr>
2019-09-24 18:31:59 +0200

    add ajax logic

--- app/controllers/privacy_notices_controller.rb
+++ app/controllers/privacy_notices_controller.rb
@@ -7,20 +7,17 @@ class PrivacyNoticesController < ApplicationController
     @privacy_notice = PrivacyNotice.new(privacy_notice_params)
     respond_to do |format|
       if @privacy_notice.save
-        format.html { redirect_to root_path, notice: 'User profil was successfully created.' }
-        format.js
+        format.html { redirect_to root_path, notice: "Privacy Notice was successfully created." }
       else
-        format.html { redirect_to root_path, notice: 'Nothing created' }
-        format.js
+        format.html { redirect_to root_path, notice: "Nothing created." }
       end
+      format.js
     end
   end
 
   private
 
   def privacy_notice_params
-    params.require(:user_profil).permit(:gender, :age, :size, :weight, :sport_frequencies, :sport_interests,
-                                        :home_address, :work_address, :avatar, :confirmation_informations,
-                                        :first_name, :last_name)
+    params.require(:privacy_notice).permit(:type_of_operation, :organizer, :collected_data, :environment, :shared_data, :reused_data)
   end
 end
